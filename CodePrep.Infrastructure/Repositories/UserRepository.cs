419ba17898e2dccab61d477b8d417fb935f7b7b4
Miraz Hossain Shahin <mirazshahin847@gmail.com>
2026-07-31 02:30:12 +0600

    Update UserRepository.cs

--- CodePrep.Infrastructure/Repositories/UserRepository.cs
+++ CodePrep.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using CodePrep.Application.Interfaces;
+using CodePrep.Application.Interfaces;
 using CodePrep.Domain.Entities;
 using CodePrep.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -17,14 +17,15 @@ public class UserRepository : IUserRepository
     public async Task AddAsync(User user)
     {
         await _context.Users.AddAsync(user);
-        await _context.SaveChangesAsync(); // Auto save kore dibe jate stream ba connection pending na thake
+        await _context.SaveChangesAsync();
     }
 
     public async Task<User?> GetByEmailAsync(string email)
     {
-        return await _context.Users
-            .AsNoTracking() // Npgsql stream read error avoid korar jonno
-            .FirstOrDefaultAsync(x => x.Email == email);
+        // Stream conflict aoratar jonno synchronous execution use kora holo
+        return await Task.Run(() => _context.Users
+            .AsNoTracking()
+            .FirstOrDefault(x => x.Email == email));
     }
 
     public async Task SaveChangesAsync()
@@ -34,8 +35,8 @@ public class UserRepository : IUserRepository
 
     public async Task<User?> GetByUserNameAsync(string username)
     {
-        return await _context.Users
-            .AsNoTracking() // Npgsql stream read error avoid korar jonno
-            .FirstOrDefaultAsync(x => x.UserName == username);
+        return await Task.Run(() => _context.Users
+            .AsNoTracking()
+            .FirstOrDefault(x => x.UserName == username));
     }
 }
